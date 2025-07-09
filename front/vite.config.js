9cb2c5491d33cbc29997a628ab585fb4c35a6ff0
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-07-09 16:48:08 +0200

    Revert "Api integration"

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import fs from 'fs'
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: ['d3385a678bed.ngrok-free.app'],
-    // https: {
-    //   key: fs.readFileSync('./localhost-key.pem'),
-    //   cert: fs.readFileSync('./localhost.pem'),
-    // },
-  }
 })
